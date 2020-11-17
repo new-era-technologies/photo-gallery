94c4e9131c440993d89a255dbc4ff3944f40b6ef
Vladymyr Radionov <radionovdon@gmail.com>
2020-11-17 21:08:18 +0200

    list img

---
+++ templateMonster/app/src/components/Item.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import './Item.scss';
+
+
+class Item extends Component {
+    render() {
+        const  {
+            props: {
+                item: {
+                    id,
+                    author,
+                    download_url
+                }
+            }
+        } = this;
+        return (
+            <div className='Item'>
+                <Link to={`/list/${id}`} key={id} className='Item__link'>
+                    <figure className='Item__fig'>
+                        <img src={download_url} alt={`img_${id}`} className='Item__fig__img' />
+                        <figcaption className='Item__fig__cap'>
+                            by {author}
+                        </figcaption>
+                    </figure>
+                </Link>
+            </div>
+        )
+    }
+}
+
+export default Item;
--- templateMonster/app/src/containers/MainList/MainList.js
+++ templateMonster/app/src/containers/MainList/MainList.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+import Item from '../../components/Item';
 import axios from 'axios';
+import './MainList.scss';
 
 
 class MainList extends Component {
@@ -20,11 +22,19 @@ class MainList extends Component {
     }
 
     render() {
-        const {state: {list}} = this;
+        const { state: { list } } = this;
         console.log(list);
         return (
             <div className='MainList'>
-
+                <h2 className='MainList__title'>
+                    list of images
+                </h2>
+                <div className='MainList__container'>
+                    {list.map(
+                        (item, i) =>
+                            <Item item={item} key={i} />
+                    )}
+                </div>
             </div>
         )
     }
